da10bd8b77316ea821c77e6abfb90ed869ef8bd6
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-07-20 21:28:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const noteContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "imges/delet.png";
+  noteContainer.appendChild(inputBox).appendChild(img);
+});
