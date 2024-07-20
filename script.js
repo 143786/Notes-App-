75f0fe92564eb593726be18b20e09a626bc8a6bb
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-07-20 22:42:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-const noteContainer = document.querySelector(".notes-container");
+const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes, notesContainer.innerHTML");
+}
 
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
   inputBox.className = "input-box";
   inputBox.setAttribute("contenteditable", "true");
-  img.src = "imges/delet.png";
-  noteContainer.appendChild(inputBox).appendChild(img);
+  img.src = "images/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+// For Delete  inputbox //
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "p") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydow", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLinerBreak");
+    event.preventDefault();
+  }
 });
